d3129064df7b1fdaed43aee1143239df44754425
David Capella <D_Capella@yahoo.com>
2017-03-08 22:30:19 -0600

    in process of finishing checkpoint-1

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,25 +1,3 @@
-/*(function() {
-  function config($stateProvider, $locationProvider) {
-    $locationProvider
-      .html5Mode({
-        enabled: true,
-        requireBase: false
-      });
-    
-    $stateProvider
-      .state('landing', {
-      url: '/',
-      controller: 'LandingCtrl as landing',
-      templateUrl: '/templates/landing.html'
-    });
-    
-  }
-  
-  angular
-    .module('blocJams', ['ui.route', 'firebase'])
-    .config(config);
-})();*/
-
 (function() {
   function config($stateProvider, $locationProvider) {
     $locationProvider
@@ -33,77 +11,11 @@
       url: '/',
       controller: 'LandingCtrl as landing',
       templateUrl: '/templates/landing.html'
-    })
-    .state('album', {
-      url: '/album',
-      controller: 'AlbumCtrl as album',
-      templateUrl: '/templates/album.html'
-    })
-    .state('collection', {
-      url: '/collection',
-      controller: 'CollectionCtrl as collection',
-      templateUrl: '/templates/collection.html'
     });
-    //edit
     
-
-/*
-    .state('player_bar', {
-      url: '/player_bar',
-      controller: 'PlayerBarCtrl as player_bar',
-      templateUrl: '/templates/player_bar.html'
-    });
-*/
-
-    
-    //end of edit
   }
   
   angular
-    .module('blocJams', ['ui.router'])
+    .module('blocChat', ['ui.route', 'firebase'])
     .config(config);
 })();
-
-/*(function() {
-  function config($stateProvider, $locationProvider) {
-    $locationProvider
-      .html5Mode({
-        enabled: true,
-        requireBase: false
-      });
-    
-    $stateProvider
-      .state('landing', {
-      url: '/',
-      controller: 'LandingCtrl as landing',
-      templateUrl: '/templates/landing.html'
-    })
-    .state('album', {
-      url: '/album',
-      controller: 'AlbumCtrl as album',
-      templateUrl: '/templates/album.html'
-    })
-    .state('collection', {
-      url: '/collection',
-      controller: 'CollectionCtrl as collection',
-      templateUrl: '/templates/collection.html'
-    });
-    //edit
-    
-
-/*
-    .state('player_bar', {
-      url: '/player_bar',
-      controller: 'PlayerBarCtrl as player_bar',
-      templateUrl: '/templates/player_bar.html'
-    });
-*/
-
-    
-    //end of edit
-  /*}
-  
-  angular
-    .module('blocJams', ['ui.router'])
-    .config(config);
-})();*/
