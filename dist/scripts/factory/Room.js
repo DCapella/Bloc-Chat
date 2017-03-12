3137db478c5b86255195f80706fed59c300b3b2a
David Capella <D_Capella@yahoo.com>
2017-03-11 22:33:12 -0600

    in progress checkpoint-3

--- dist/scripts/factory/Room.js
+++ dist/scripts/factory/Room.js
@@ -2,7 +2,11 @@
   function Room($firebaseArray) {
     var ref = firebase.database().ref().child("rooms");
     var rooms = $firebaseArray(ref);
-    
+    rooms.$add().then(function(ref) {
+      var id = ref.key;
+      console.log("added record with id " + id);
+      rooms.$indexFor(id);
+    });
     return {
       all: rooms
     };
