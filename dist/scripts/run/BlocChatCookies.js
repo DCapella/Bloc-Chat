6f35090bcc60fc1be3b57b394419e56d0e84ddab
David Capella <D_Capella@yahoo.com>
2017-03-17 22:36:09 -0500

    end of checkpoint-5

---
+++ dist/scripts/run/BlocChatCookies.js
@@ -0,0 +1,21 @@
+(function() {
+  function BlocChatCookies($cookies, $uibModal) {
+    $cookies.remove('blocChatCurrentUser');
+    var currentUser = $cookies.get('blocChatCurrentUser');
+    if (!currentUser || currentUser === '') {
+      $uibModal.open({
+        // Modal configuration object properties
+        keyboard: false,
+        templateUrl: '/templates/modalUsername.html',
+        controller: 'ModalUsernameCtrl as modalUsername'        
+      });
+      // Do something to allow users to set their username
+    } else {
+      alert("Your username is " + currentUser);
+    }
+  }
+  
+  angular
+    .module('blocChat')
+    .run(['$cookies', '$uibModal', BlocChatCookies]);
+})();
