cf42b3c721e95486fe94129346212745f1fe0225
David Capella <D_Capella@yahoo.com>
2017-03-18 23:15:22 -0500

    end of checkpoint-6

--- dist/scripts/factory/Message.js
+++ dist/scripts/factory/Message.js
@@ -3,14 +3,32 @@
     var ref = firebase.database().ref().child("messages");
     var messages = $firebaseArray(ref);
     
-/*
-    console.log($firebaseArray(ref.orderByChild("roomId").equalTo("room1")));
-*/
+    var now = function() {
+      var d = new Date();
+      var h = d.getHours();
+      var m = d.getMinutes();
+      
+      if (h < 10) {
+        h = "0" + h;
+      }
+      if (m < 10) {
+        m = "0" + m;
+      }
+      
+      var time = h + "" + m;
+      
+      return time;
+    }
     
     return {
       getByRoomId: function (roomId) {
         // Filter the mesages by their room ID.
         return $firebaseArray(ref.orderByChild("roomId").equalTo(roomId));
+      },
+      send: function(newMessage, roomId, username) {
+        // Send method logic
+        var time = now();
+        messages.$add({ content: newMessage, roomId: roomId, sentAt: time, username: username });
       }
     };
   }
